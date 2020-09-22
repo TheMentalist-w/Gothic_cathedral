75989815877861c522e8d53abaa935e59197f83a
TheMentalist-w <weronika.mro@interia.pl>
2020-09-22 10:27:29 +0200

    light change

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -3,33 +3,40 @@
 
 uniform sampler2D textureMap0;
 
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
+out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny kolor piksela
 
 //Zmienne interpolowane
-in vec4 n;
-in vec4 l;
+in vec4 l_0;
+in vec4 l_1;
 in vec4 lo;
+in vec4 n;
 in vec4 v;
 in vec2 iTexCoord0;
 
 void main(void) {
-	vec4 ml = normalize(l);
+
+	//ponowna normalizacja zmiennych interpolowanych
+	vec4 ml_0 = normalize(l_0);
+	vec4 ml_1 = normalize(l_1);
 	vec4 mlo = normalize(lo);
 	vec4 mn = normalize(n);
 	vec4 mv = normalize(v);
 
-	//wektory odbite
-	vec4 mr = reflect(-ml, mn);
+	//wektory odbite w przestrzeni oka
+	vec4 mr_0 = reflect(-ml_0, mn);
+	vec4 mr_1 = reflect(-ml_1, mn);
 	vec4 mro = reflect(-mlo, mn);
 
 	vec4 kd = texture(textureMap0, iTexCoord0);
 	vec4 ks = kd;
 
-
-	float nl = clamp(dot(mn, ml), 0, 1);
+	//iloczyny skalarne
+	float nl_0 = clamp(dot(mn, ml_0), 0, 1);
+	float nl_1 = clamp(dot(mn, ml_1), 0, 1);
 	float nlo = clamp(dot(mn, mlo), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 25);
-	float rvo = pow(clamp(dot(mro, mv), 0, 1), 25);
+	float rv_0 = pow(clamp(dot(mr_0, mv), 0, 1), 5);
+	float rv_1 = pow(clamp(dot(mr_1, mv), 0, 1), 5);
+	float rvo = pow(clamp(dot(mro, mv), 0, 1), 25); //25 - skupienie œwiat³a
 
-	pixelColor = vec4(kd.rgb * nl*vec3(1, 0.9, 0.9), kd.a) + vec4(ks.rgb *rv*vec3(1, 0.9, 0.9), 0) + vec4(kd.rgb * nlo*vec3(0.83, 0.69, 0.1686), kd.a) + vec4(ks.rgb *rvo * vec3(0.83, 0.69, 0.1686), 0);
+	pixelColor = vec4(kd.rgb * nl_0, kd.a) + vec4(ks.rgb *rv_0, 0) + vec4(kd.rgb * nl_1, kd.a) + vec4(ks.rgb *rv_1, 0) + vec4(kd.rgb * nlo*vec3(0.83, 0.69, 0.1686), kd.a) + vec4(ks.rgb *rvo * vec3(0.83, 0.69, 0.1686), 0);
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -14,19 +14,29 @@ in vec2 texCoord0;
 
 
 //Zmienne interpolowane
-out vec4 l;
+out vec4 l_0;
+out vec4 l_1;
 out vec4 lo;
 out vec4 n;
 out vec4 v;
 out vec2 iTexCoord0;
 
 void main(void) {
+	//Ÿród³a œwiat³a
 	vec4 lpo = vec4(1.5, 0, 0, 1); // pochodnia
-	vec4 lp = vec4(-17, -16, -0.2, 1); // o³tarz
+	vec4 lp_0 = vec4(0, 13, -19, 1); // œwiat³o g³ówne
+	vec4 lp_1 = vec4(0, 13, 0, 1); // œwiat³o g³ówne 1
+
+
 	lo = normalize(lpo - V * M * vertex);
-	l = normalize(V * M * lp - V * M * vertex);
-	n = normalize(vec4(0, 0, 0, 1) - V * M * vertex);
-	v = normalize(V * M * normal);
+	l_0 = normalize(V * lp_0 - V * M * vertex);
+	l_1 = normalize(V * lp_1 - V * M * vertex);
+
+	n = normalize(V * M * normal);
+
+	//wektor do obserwatora
+	v = normalize(vec4(0, 0, 0, 1) - V * M * vertex);
+
 	iTexCoord0 = texCoord0;
 
     gl_Position=P*V*M*vertex;
