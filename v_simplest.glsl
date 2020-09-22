458b6f9e2e8231e8ea5a247c3b999d9c3289d49c
TheMentalist-w <weronika.mro@interia.pl>
2020-09-22 10:52:45 +0200

    .

--- v_simplest.glsl
+++ v_simplest.glsl
@@ -24,13 +24,13 @@ out vec2 iTexCoord0;
 void main(void) {
 	//Ÿród³a œwiat³a
 	vec4 lpo = vec4(1.5, 0, 0, 1); // pochodnia
-	vec4 lp_0 = vec4(0, 13, -19, 1); // œwiat³o g³ówne
-	vec4 lp_1 = vec4(0, 13, 0, 1); // œwiat³o g³ówne 1
+	vec4 lp_0 = vec4(10, 23, 0, 1); // œwiat³o g³ówne
+	vec4 lp_1 = vec4(-10, 23, 0, 1); // œwiat³o g³ówne 1
 
 
 	lo = normalize(lpo - V * M * vertex);
-	l_0 = normalize(V * lp_0 - V * M * vertex);
-	l_1 = normalize(V * lp_1 - V * M * vertex);
+	l_0 = normalize(lp_0 - V * M * vertex);
+	l_1 = normalize(lp_1 - V * M * vertex);
 
 	n = normalize(V * M * normal);
 
